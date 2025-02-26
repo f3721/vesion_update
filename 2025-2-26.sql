4e22f9a5a20086f5c4e35064dae2afac0a5f3f0c
f3721 <songxin021@gmail.com>
2025-02-26 10:21:25 +0800

    Update 2025-2-26.sql

--- 2025-2-26.sql
+++ 2025-2-26.sql
@@ -1,6 +1,3 @@
 //sql更新
 ALTER TABLE `x_tweet` 
-ADD COLUMN `j_photo_num` int NULL DEFAULT 0 COMMENT 'json图片数量' AFTER `type`,
-MODIFY COLUMN `photo` int NULL DEFAULT 0 COMMENT '图片' AFTER `type`,
-ADD COLUMN `j_video_num` int NULL DEFAULT 0 COMMENT 'json视频数量' AFTER `photo`,
-MODIFY COLUMN `video` int NULL DEFAULT 0 COMMENT 'mp4数量' AFTER `photo`;
+ADD COLUMN `json_media_num` int NULL DEFAULT 0 COMMENT '媒体数量包含图片和视频' AFTER `type`;
