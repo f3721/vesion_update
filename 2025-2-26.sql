50d1e658e88834980ea995de748b6b8d0b76577c
f3721 <songxin021@gmail.com>
2025-02-27 15:32:52 +0800

    Update 2025-2-26.sql

--- 2025-2-26.sql
+++ 2025-2-26.sql
@@ -7,3 +7,7 @@ ADD COLUMN `json_media_num` int NULL DEFAULT 0 COMMENT '媒体数量包含图片
 //2025-02-27
 ALTER TABLE `app_article_history` 
 MODIFY COLUMN `cover_resource_id` bigint NULL COMMENT '封面id' AFTER `types`;
+
+
+ALTER TABLE `x_fan` 
+ADD COLUMN `f_type` int NULL DEFAULT 1 COMMENT '粉类型：1是twitter，2小视频' AFTER `fanid`;
