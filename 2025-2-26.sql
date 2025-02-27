f93b5e1f524dd598b57c5d8380c474243a79c40a
f3721 <songxin021@gmail.com>
2025-02-27 15:16:40 +0800

    Update 2025-2-26.sql

--- 2025-2-26.sql
+++ 2025-2-26.sql
@@ -1,3 +1,9 @@
 //sql更新
 ALTER TABLE `x_tweet` 
 ADD COLUMN `json_media_num` int NULL DEFAULT 0 COMMENT '媒体数量包含图片和视频' AFTER `type`;
+
+
+
+//2025-02-27
+ALTER TABLE `app_article_history` 
+MODIFY COLUMN `cover_resource_id` bigint NULL COMMENT '封面id' AFTER `types`;
